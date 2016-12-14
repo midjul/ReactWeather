13f8d30e1c95f723b0d96d05668e142e54cecdb8
Midhet Đulović <djulovic_m@hotmail.com>
2016-12-14 17:24:05 +0100

    Server fix

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var express = require('express');
 var app = express();
 const PORT=process.env.PORT || 3000;
 app.use(function(req, res, next){
-if (req.headers('x-forwarder-proto')==='http') {
+if (req.headers['x-forwarded-proto']==='http') {
 next();
 } else {
-res.redirect('http://'+req.hostname+req.url);  
+res.redirect('http://'+req.hostname+req.url);
 }
 });
 app.use(express.static('public'));
